c5762400cf8e6ece57df5d7d3dfb6176c841906c
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2025-12-27 01:13:16 +0530

    Initial commit

---
+++ src/pages/jobListing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobListing = () => {
+  return (
+    <div>J</div>
+  )
+}
+
+export default JobListing
---
+++ src/pages/post-job.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostJob = () => {
+  return (
+    <div>post-job</div>
+  )
+}
+
+export default PostJob
